4c01b50026e3404cc62932c79a09b3035192f752
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-03-05 22:50:54 -0600

    changing 2space to 4 space tab

--- src/components/header.js
+++ src/components/header.js
@@ -11,23 +11,14 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
-
-
-type Props = {};
-export default class App extends Component<Props> {
+type Header = {};
+export default class App extends Component<Header> {
     render() {
         return (
             <View style={styles.container}>
                 <Text>Some Text</Text>
                 <Text style={styles.welcome}>Welcome to React Native!</Text>
                 <Text style={styles.instructions}>To get started, edit App.js</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
             </View>
     );
   }
